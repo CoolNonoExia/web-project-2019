ee6df2a52c360b37d5d0b3c1be6fe6c9cd674f40
Lehoux <quentin.lehoux@viacesi.fr>
2019-01-22 13:51:32 +0100

    Js

--- resources/views/pages/evenement.blade.php
+++ resources/views/pages/evenement.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.default')
 @section('content')
-    i am the evenement page
+
+        <div class="checkbox">
+            <label><input type="checkbox" value="true"> Evénement passés</label>
+        </div>
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown button
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </div>
+
 @stop
