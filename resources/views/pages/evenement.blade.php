64441d9ff983fffe7d1e333dfdd8cb0ab30d96ba
Lehoux <quentin.lehoux@viacesi.fr>
2019-01-22 14:13:58 +0100

    Js

--- resources/views/pages/evenement.blade.php
+++ resources/views/pages/evenement.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.default')
 @section('content')
 
-        <div class="checkbox">
+        <span class="checkbox">
             <label><input type="checkbox" value="true"> Evénement passés</label>
-        </div>
-        <div class="dropdown">
+        </span>
+        <span class="dropdown float-right ">
+            <span>Trier par :</span>
             <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown button
+                Date
             </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton" >
+                <a class="dropdown-item" href="#">Payant</a>
+                <a class="dropdown-item" href="#">Gratuit</a>
+                <a class="dropdown-item" href="#">Nom</a>
             </div>
-        </div>
+        </span>
 
 @stop
