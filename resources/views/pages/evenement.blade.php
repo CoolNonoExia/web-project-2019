d5706f44846a12b86deab7329d8d4ac18101d583
CoolNonoExia <nolan.amouyal@viacesi.fr>
2019-01-23 10:44:27 +0100

    Changed event page

--- resources/views/pages/evenement.blade.php
+++ resources/views/pages/evenement.blade.php
@@ -5,13 +5,9 @@
 @section('content')
 
        <div class="container-fluid">
-           <div class="row" style="margin-bottom: 40px;">
-               <div class="col-3">
-                   {{--IMAGE--}}
-               </div>
-               <div class="col-6">
+           <div class="row">
+               <div class="col-8 offset-2">
                    <div class="row">
-
                        <div class="checkbox col-4">
                            <label><input type="checkbox" value="true"> Evénement passés</label>
                        </div>
@@ -27,44 +23,39 @@
                            </div>
                        </div>
                    </div>
-               </div>
-
-           </div>
-
-           <div>
-               <div class="col mx-auto" style="background-color: #BD0F14; color: white; font-weight: bold; padding:4px">
-
-                   <p> Evenement à venir </p>
-
+                   <div class="col mx-auto" style="margin: 30px 0px; background-color: #BD0F14; color: white; font-weight: bold; padding:4px">
+                       <p> Evenement à venir </p>
+                   </div>
                </div>
            </div>
+
            <div class="row">
-               <div class="col">
-                   <p style="font-weight: bold;"> {{($event['title'])}}</p>
-               </div>
                <div class="col-2">
-                   <p>
-                       {{$event['events_date']}}
-                   </p>
+                   {{-- Images --}}
+               </div>
+               <div class="col-8">
+                   <div class="row">
+                       <div class="col">
+                           <span style="font-weight: bold;"> {{($event['title'])}}</span>
+                       </div>
+                       <div class="col text-right">
+                           <span>
+                               {{$event['events_date']}}
+                           </span>
+                       </div>
+                   </div>
+                   <div class="border">
+                       <div>{{$event['description']}}</div>
+                       <div class="text-right">
+                           @if($event['is_free']==0)
+                               Free
+                           @else
+                               Payant
+                           @endif
+                       </div>
+                   </div>
+                   <hr style="border-top: 1px dashed #8c8b8b;">
                </div>
            </div>
-            <div class="border">
-                <div class="row">
-                    <div class="col">
-                        <p>{{$event['description']}}</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12 text-right">
-                        @if($event['is_free']==0)
-                            Free
-                        @else
-                            Payant
-                        @endif
-                    </div>
-                </div>
-
-            </div>
-           <hr style="border-top: 1px dashed #8c8b8b;">
        </div>
 @stop
