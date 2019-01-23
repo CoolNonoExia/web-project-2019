ac957c25a99d565f4dcb3746d37cc22f38811e80
Lehoux <quentin.lehoux@viacesi.fr>
2019-01-23 10:29:20 +0100

    Event -BDD CSS

--- resources/views/pages/evenement.blade.php
+++ resources/views/pages/evenement.blade.php
@@ -4,25 +4,31 @@
 @endsection
 @section('content')
 
-       <div class="container">
-           <div style="margin-bottom: 40px;">
-               <div class="row">
+       <div class="container-fluid">
+           <div class="row" style="margin-bottom: 40px;">
+               <div class="col-3">
+                   {{--IMAGE--}}
+               </div>
+               <div class="col-6">
+                   <div class="row">
 
-                   <div class="checkbox col-4">
-                       <label><input type="checkbox" value="true"> Evénement passés</label>
-                   </div>
-                   <div class="dropdown  text-right col-8 ">
-                       <span>Trier par : </span>
-                       <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                           Date
-                       </button>
-                       <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" >
-                           <a class="dropdown-item" href="#">Payant</a>
-                           <a class="dropdown-item" href="#">Gratuit</a>
-                           <a class="dropdown-item" href="#">Nom</a>
+                       <div class="checkbox col-4">
+                           <label><input type="checkbox" value="true"> Evénement passés</label>
+                       </div>
+                       <div class="dropdown  text-right col-8 ">
+                           <span>Trier par : </span>
+                           <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                               Date
+                           </button>
+                           <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" >
+                               <a class="dropdown-item" href="#">Payant</a>
+                               <a class="dropdown-item" href="#">Gratuit</a>
+                               <a class="dropdown-item" href="#">Nom</a>
+                           </div>
                        </div>
                    </div>
                </div>
+
            </div>
 
            <div>
@@ -57,7 +63,8 @@
                         @endif
                     </div>
                 </div>
-            </div>
 
+            </div>
+           <hr style="border-top: 1px dashed #8c8b8b;">
        </div>
 @stop
