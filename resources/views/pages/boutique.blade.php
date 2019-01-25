8ceea935da5ec6b853175d3d1f190fae050a6a1f
Lehoux <quentin.lehoux@viacesi.fr>
2019-01-25 14:36:40 +0100

    Liste product arrangée

--- resources/views/pages/boutique.blade.php
+++ resources/views/pages/boutique.blade.php
@@ -53,24 +53,20 @@
             <li data-target="#carousel-home" data-slide-to="0" class="active"></li>
             <li data-target="#carousel-home" data-slide-to="1"></li>
             <li data-target="#carousel-home" data-slide-to="2"></li>
-            <li data-target="#carousel-home" data-slide-to="3"></li>
+
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="./images/sweat.png" alt="First slide" height="">
+                <img class="d-block w-100" src='{{asset('/images/sweat.png')}}' alt="First slide" height="">
                 <div class="carousel-caption d-none d-md-block" style="color: #9b000c"><b> Article 1: ...€ </b></div>
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="./images/sweat.png" alt="Second slide">
+                <img class="d-block w-100" src='{{asset('/images/sweat.png')}}' alt="Second slide">
                 <div class="carousel-caption d-none d-md-block" style="color: #9b000c"><b> Article 2: ...€ </b></div>
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="./images/sweat.png" alt="Third slide">
+                <img class="d-block w-100" src='{{asset('/images/sweat.png')}}' alt="Third slide">
                 <div class="carousel-caption d-none d-md-block" style="color: #9b000c"><b> Article 3: ...€</b></div>
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="./images/sweat.png" alt="Fourth slide">
-                <div class="carousel-caption d-none d-md-block" style="color: #9b000c"><b> Article 4: ...€ </b></div>
             </div>
         </div>
         <a class="carousel-control-prev" href="#carousel-home" role="button" data-slide="prev">
@@ -118,12 +114,12 @@
         </div>
         <div class="col-8">
             <div class="row">
-            @foreach($product as $products)
+            @foreach($products as $product)
             <div class="col">
                 <img class="img-fluid" src='{{asset('/images/sweat.png')}}' alt="Responsive image">
-                <p style="text-align: center">  {{$products['name']}} </p>
-                <p>{{$products['description']}}</p>
-                <p> Get this product for only {{$products['price']}}€ </p>
+                <p style="text-align: center">  {{$product['name']}} </p>
+                <p>{{$product['description']}}</p>
+                <p> Get this product for only {{$product['price']}}€ </p>
                 <button class="btn btn-blue"> Ajouter au panier </button>
                 <p></p>
             </div>
