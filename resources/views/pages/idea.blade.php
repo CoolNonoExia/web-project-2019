5e442ffb89f73a03f74e946b4b3c5f8a44d780e5
AlphaSly <33154299+AlphaSly@users.noreply.github.com>
2019-01-28 14:24:37 +0100

    idee

--- resources/views/pages/idea.blade.php
+++ resources/views/pages/idea.blade.php
@@ -18,40 +18,33 @@
     <hr>
 
     <div class="row justify-content-center">
-        <div class="col-9" style="margin: 30px 0px; background-color: #BD0F14; color: white; font-weight: bold; padding:4px">
+        <div class="col-9" style="margin: 30px 0px; background-color: #1b4b72; color: white; font-weight: bold; padding:4px">
             <p>Evenements proposés</p>
         </div>
     </div>
 
     <hr>
 
-    <p><b>Proposer un évènement</b></p>
+    <p style="text-align: center"><b>Proposer un évènement</b></p>
     <form method="post" action="idea.blade.php" enctype="multipart/form-data">
-        <label for="title"><b>Titre de l'évènement></b></label><br>
+        <label for="title"><b>Titre de l'évènement</b></label>
     <input type="text" name="title" value="titre" /><br>
-        <label for="description"><b>Description de l'évènement</b></label><br>
+        <label for="description"><b>Description de l'évènement</b></label>
     <input type="text" name="description" value="description" /><br>
     <input type="hidden" name="max_size" value="1048576" />
-        <label type="image"><b>Illustration évènement</b></label><br>
-    <input type="file" name="image" value="image" /><br><br>
-    <input class="btn btn-outline-blue" type="submit" name="submit" value="Envoyer" />
+        <label type="image"><b>Illustration évènement</b></label>
+    <input type="file" name="image" value="image" /><br>
+    <input style="float: right;"class="btn btn-outline-blue" type="submit" name="submit" value="Proposer" />
     </form>
 
     <!--?php
-        $FILES['']
+        $_FILES['']
         if ($_FILES[''][''] > 0) $erreur = "Erreur lors du transfert";
         if ($_FILES[''][''] > $maxsize) $erreur = "Le fichier est trop gros";
         $extension_valides = array('jpg','jpeg','gif','png');
         $extension_upload = strtolower( substr( strrchr($_FILES[''][''], '.') ,1));
         if (in_array($extension_upload,$extension_valides)) echo = "Extension correcte";
-
         $image_sizes = getimagesize($_FILES['']['']);
-        if ($image_sizes[0] > $maxwidth OR $image_sizes[1] > $maxheight) $erreur = "Image trop grande";
-
-    vérification des erreurs
-
-    besoin de remplir les [''] suivre ce lien https://openclassrooms.com/fr/courses/1085676-upload-de-fichiers-par-formulaire
-
 
     ?-->
 
