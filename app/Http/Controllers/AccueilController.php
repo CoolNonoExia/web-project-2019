1488a33ac5ea5bc2430fbcf0bcbacbe0c2573569
CoolNonoExia <nolan.amouyal@viacesi.fr>
2019-01-29 22:11:03 +0100

    Petit fix

--- app/Http/Controllers/AccueilController.php
+++ app/Http/Controllers/AccueilController.php
@@ -10,31 +10,4 @@ class AccueilController extends Controller
     {
         return view('pages.home');
     }
-
-    //TODO: Delete this later (test)
-    public function testpost()
-    {
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('POST', 'http://localhost:3000/users', [
-            'form_params' => [
-                'last_name' => 'krunal',
-                'first_name' => 'krunal',
-                'email' => 'krunal',
-                'password' => 'krunal',
-                'id_role' => 'krunal',
-                'id_campus' => 'krunal',
-            ]
-        ]);
-        $response = $response->getBody()->getContents();
-        echo '<pre>';
-        print_r($response);
-    }
-    public function testget()
-    {
-        $client = new \GuzzleHttp\Client();
-        $request = $client->get('http://localhost:3000/test');
-        $response = $request->getBody()->getContents();
-        echo '<pre>';
-        print_r($response);
-    }
 }
