b96305a88224bfa433ff8c08a016512ea4364b76
Lehoux <quentin.lehoux@viacesi.fr>
2019-01-30 09:04:07 +0100

    Home

--- app/Http/Controllers/AccueilController.php
+++ app/Http/Controllers/AccueilController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\EventModel;
+use App\Image_events;
 
 class AccueilController extends Controller
 {
     public function index()
     {
-        return view('pages.home');
+        $events = EventModel::all()->where('events_date', '>', date('Y-m-d h:i:s', time()))->sortBy('title');
+        $imgs = Image_events::all();
+        $active = true;
+        
+        return view('pages.home',['events' => $events,'imgs'=> $imgs, 'active'=>$active]);
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -14,38 +14,55 @@
 @endsection
 
 @section('content')
-    <div id="carousel-home" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carousel-home" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-home" data-slide-to="1"></li>
-            <li data-target="#carousel-home" data-slide-to="2"></li>
-            <li data-target="#carousel-home" data-slide-to="3"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block w-100" src="{{ asset('svg/403.svg') }}" alt="First slide">
-                <div class="carousel-caption d-none d-md-block">image 1</div>
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="{{ asset('svg/404.svg') }}" alt="Second slide">
-                <div class="carousel-caption d-none d-md-block">image 2</div>
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="{{ asset('svg/500.svg') }}" alt="Third slide">
-                <div class="carousel-caption d-none d-md-block">image 3</div>
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="{{ asset('svg/503.svg') }}" alt="Fourth slide">
-                <div class="carousel-caption d-none d-md-block">image 4</div>
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carousel-home" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carousel-home" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+    <div>
+        <p> Le bureau des élèves ou BDE est une association qui anime la vie étudiante de son école ou son université. Ce bureau est composé d'étudiants élus par leurs camarades et qui les représentent auprès de l'administration. </p>
+        <br>
+        <p> Ici vous trouverez une splendide page Evenement, regroupant tous les événements passées et à venir organisé par le BDE. </p>
+        <p>Vous pouvez vous inscrire aux événements qui vous intérèsse. Vous puovez également commenter, liker les événements auxquelles vous avez participés</p>
+        <br>
+        <p> Vous trouverez une page boutique complète, proposant une gamme variée de produit fait par votre BDE</p>
+        <br>
+        <p> Vous avez des idées d'évenements ? Faites un tour dans la section boîte à idées et proposer la votre !   </p>
+        <p> Vous pouvez aussi votez pour les idées proposées par les autres étudiants.</p>
+        <p> Les idées avec le plus de votes seront surement selectionnée par le BDE et organisée ! Alors n'hésitez pas ! </p>
+        <br>
+        <p> Vous trouvez ci dessus un lien menant aux mentions légales du site. Un bouton représenté par une balance se trouve dans le footer pour afficher cette même page</p>
+        <a href="{{route('legal')}}"> Mention légal</a>
     </div>
+    <div class="row justify-content-center">
+        <div class="col-9" style="margin: 30px 0px; background-color: #BD0F14; color: white; font-weight: bold; padding:4px">
+            <p>Evenement à venir</p>
+        </div>
+    </div>
+    <div class="row">
+        <div id="carousel-home" class="carousel slide col-4 offset-4" data-ride="carousel";>
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-home" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-home" data-slide-to="1"></li>
+                <li data-target="#carousel-home" data-slide-to="2"></li>
+            </ol>
+
+            <div class="carousel-inner">
+                @foreach($events as $event)
+                    <div class="carousel-item {{$active ? 'active' : ''}}" >
+                        <?php $img = $imgs->find($event['id_images_events']) ?>
+                        <img class="d-block w-100" src="{{asset('storage\\img\\events\\'.$img['id'].'.'.$img['ext'])}}" style="height: 200px";>
+                        <div class="carousel-caption d-none d-md-block" style="color: #9b000c"></div>
+                    </div>
+                    <?php $active = false; ?>
+                @endforeach
+            </div>
+
+            <a class="carousel-control-prev" href="#carousel-home" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+
+            <a class="carousel-control-next" href="#carousel-home" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
 @endsection
